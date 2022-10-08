29aaa8a313d0aa0b3c5869c03c64e86331293249
QuentinRCR <reyquentin.rq@gmail.com>
2022-10-08 16:28:13 +0200

    Sprint boot initialisation

---
+++ src/main/java/com/docto/protechdoctolib/ProtechDoctolibApplication.java
@@ -0,0 +1,12 @@
+package com.docto.protechdoctolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtechDoctolibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtechDoctolibApplication.class, args);
+	}
+}
---
+++ src/test/java/com/docto/protechdoctolib/ProtechDoctolibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docto.protechdoctolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtechDoctolibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
