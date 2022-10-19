2203564a76f76b1eb1ef13b8950e0fbeeb2c7c9c
Matteo LONGO <matteo.longo@etu.emse.fr>
2022-10-19 16:14:10 +0200

    create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO RENDEZ_VOUS(id,date_Debut, idUser, idCreneau,duree, moyenCommunication, zoomLink) VALUES(1,{ts '2022-10-16 00:00:00.00'}, 2, 3,40,'zoom','link.fr');
+
+
+INSERT INTO RENDEZ_VOUS(id,date_Debut, idUser, idCreneau,duree, moyenCommunication, zoomLink) VALUES(2,{ts '2022-09-16 00:00:00.00'}, 3, 4,50,'zoom','link.fr');
