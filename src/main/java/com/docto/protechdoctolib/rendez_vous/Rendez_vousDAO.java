5b1071ea3d22af847ba8a8597aa4f98a9372367b
Matteo LONGO <matteo.longo@etu.emse.fr>
2022-10-19 14:31:08 +0200

    Create rdvDAO

---
+++ src/main/java/com/docto/protechdoctolib/rendez_vous/Rendez_vousDAO.java
@@ -0,0 +1,9 @@
+package com.docto.protechdoctolib.rendez_vous;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+
+public interface Rendez_vousDAO extends JpaRepository<Rendez_vous, Long> {
+
+}
