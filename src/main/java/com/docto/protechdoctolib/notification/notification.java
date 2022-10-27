c38d33cecd58ec57114f48faa6eeb1602fa07210
Abdel Wedoud Talhatte <abdelwedoud.talhatte@etu.emse.fr>
2022-10-27 16:22:35 +0200

    Notification sms

---
+++ src/main/java/com/docto/protechdoctolib/notification/notification.java
@@ -0,0 +1,47 @@
+package com.docto.protechdoctolib.notification;
+
+import javax.swing.*;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class notification {
+    public static void sendSms() {
+        try {
+            // Construct data
+            String apiKey = "apikey=" + "NmY2OTdhNjc0MzU5MzI0ZDRlNGE2NDU2NmM0NDYyNmI=";
+            String message = "&message=" + "It works";
+            String sender = "&sender=" + "Protech";
+            String numbers = "&numbers=" + "33766505462";
+
+            // Send data
+            HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
+            String data = apiKey + numbers + message + sender;
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+            conn.getOutputStream().write(data.getBytes("UTF-8"));
+            final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                stringBuffer.append(line);
+                JOptionPane.showMessageDialog(null, "message"+line);
+            }
+            rd.close();
+
+            //return stringBuffer.toString();
+        } catch (Exception e) {
+            //System.out.println("Error SMS " + e);
+            JOptionPane.showMessageDialog(null, e);
+            //return "Error "+e;
+        }
+
+    }
+
+    public static void main (String[] args){
+        sendSms();
+    }
+}
