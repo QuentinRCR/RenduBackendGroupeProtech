2fd8c673df0f0627491e9f17d503ecf42a5d315b
Matteo LONGO <matteo.longo@etu.emse.fr>
2022-11-04 17:44:09 +0100

    putting negative ID to autopopulation

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO RENDEZ_VOUS(id,date_Debut, id_User, id_Creneau,duree, moyen_Communication, zoom_Link) VALUES(1,{ts '2022-10-16 00:00:00.00'}, 2, 3,40,'zoom','link.fr');
+INSERT INTO RENDEZ_VOUS(id,date_Debut, id_User, id_Creneau,duree, moyen_Communication, zoom_Link) VALUES(-1,{ts '2022-10-16 00:00:00.00'}, 2, 3,40,'zoom','link.fr');
 
 
-INSERT INTO RENDEZ_VOUS(id,date_Debut, id_User, id_Creneau,duree, moyen_Communication, zoom_Link) VALUES(2,{ts '2022-09-16 00:00:00.00'}, 3, 4,50,'zoom','link.fr');
+INSERT INTO RENDEZ_VOUS(id,date_Debut, id_User, id_Creneau,duree, moyen_Communication, zoom_Link) VALUES(-2,{ts '2022-09-16 00:00:00.00'}, 3, 4,50,'zoom','link.fr');
