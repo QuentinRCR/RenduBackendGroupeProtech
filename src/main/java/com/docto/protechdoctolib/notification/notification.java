250afe9cca565697c275ab9577bfc8252b12fcc0
Abdel Wedoud Talhatte <abdelwedoud.talhatte@etu.emse.fr>
2022-11-09 15:36:22 +0100

    Notification sms

--- src/main/java/com/docto/protechdoctolib/notification/notification.java
+++ src/main/java/com/docto/protechdoctolib/notification/notification.java
@@ -1,47 +1,28 @@
 package com.docto.protechdoctolib.notification;
 
-import javax.swing.*;
-import java.net.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 
+@Service
 public class notification {
-    public static void sendSms() {
-        try {
-            // Construct data
-            String apiKey = "apikey=" + "NmY2OTdhNjc0MzU5MzI0ZDRlNGE2NDU2NmM0NDYyNmI=";
-            String message = "&message=" + "It works";
-            String sender = "&sender=" + "Protech";
-            String numbers = "&numbers=" + "33766505462";
+    @Autowired
+    private JavaMailSender mailSender;
 
-            // Send data
-            HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
-            String data = apiKey + numbers + message + sender;
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-            conn.getOutputStream().write(data.getBytes("UTF-8"));
-            final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            final StringBuffer stringBuffer = new StringBuffer();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                stringBuffer.append(line);
-                JOptionPane.showMessageDialog(null, "message"+line);
-            }
-            rd.close();
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("protechdocto@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
 
-            //return stringBuffer.toString();
-        } catch (Exception e) {
-            //System.out.println("Error SMS " + e);
-            JOptionPane.showMessageDialog(null, e);
-            //return "Error "+e;
-        }
+        mailSender.send(message);
 
+        System.out.println("Email sent successfully ! ");
     }
-
     public static void main (String[] args){
-        sendSms();
     }
 }
